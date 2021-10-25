2f702077aeed8a048de3265cf685815ab524ff04
Aalto <351401563@qq.com>
2021-10-25 10:58:55 +0800

    first commit

---
+++ grayflask/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+"""Script for Public Cloud
+@author: A
+"""
--- grayflask/main.py
+++ grayflask/main.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
 """Script for Public Cloud
 @author: A
 """
-
+#!/usr/bin/python3
 
 import argparse
 import numpy as np
